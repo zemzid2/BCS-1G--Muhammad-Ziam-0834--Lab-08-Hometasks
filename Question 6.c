7a72e8b0a8f6b55b601e99f142efaf103bb68369
Muhammad Ziam <k250834@nu.edu.pk>
2025-10-29 11:17:16 -0700

    Refactor main function for better output formatting
    Refactored the main function to improve formatting and output.

--- Question 6.c
+++ Question 6.c
@@ -1,33 +1,31 @@
-#include<stdio.h>
-int main(){
-	
-	int rows=5;
-	for(int i=0; i<rows; i++){
-		
-		for(int space = 0; space< (rows-1-i); space++){
-			printf(" ");
-		}
-		for(int j=0; j<=i; j++){
-			printf("*");
-		}
-		
-		printf("|\t");
-		for(int space = 0; space< (rows-1-i); space++){
-		printf(" ");
-		}
-		for(int j=0; j<=i; j++){
-		printf("%d", i+1);
-		}
-		
-		printf("|\t");
-		
-		for(int space = 0; space< (rows-1-i); space++){
-			printf(" ");
-		}
-		for(int j=0; j<=i; j++){
-		printf("%c", 'A'+1);
-		}
-		
-		printf("\n");
-	}return 0;
+#include <stdio.h>
+int main() {
+    int n = 5;
+    int i, j, k;
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++) {
+            printf("*");
+        }
+        printf("   |   ");
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++) {
+            printf("%d", k);
+        }
+        printf("   |   ");
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++) {
+            printf("%c", 'A' + k - 1);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
